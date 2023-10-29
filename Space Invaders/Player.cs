586015ec76e665e9b247aef9314c5f781066c52c
Dominik Sebe <dominiksebe9@gmail.com>
2023-10-29 15:57:27 +0100

    Created Player class.

---
+++ Space Invaders/Player.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Space_Invaders
+{
+    /// <summary>
+    /// Class that defined the player character in the game.
+    /// </summary>
+    internal class Player : Entity
+    {
+        #region Delegates
+        public delegate void AttackEvent(object player);
+        #endregion
+
+        #region Members
+        private Character[] characters;
+        private int x, y;
+        private AttackEvent attack;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets or sets the array characters that belong to the object.
+        /// </summary>
+        public override Character[] Characters 
+        { 
+            get
+            {
+                return this.characters;
+            } 
+            protected set
+            {
+                this.characters = value;
+            }
+        }
+        /// <summary>
+        /// Gets or sets a Character, that belongs to the object.
+        /// </summary>
+        /// <param name="index">The 0-based index of the element to get or set.</param>
+        /// <returns>The character object of the specified index.</returns>
+        /// <exception cref="IndexOutOfRangeException">Thrown when the specified index points outside of the array of Characters of the object</exception>
+        public override Character this[int index] 
+        { 
+            get
+            {
+                return this.characters[index];
+            }
+            set
+            {
+                this.characters[index] = value;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the position of the object on the X axis.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the value is less than zero.</exception>
+        public override int X 
+        { 
+            get
+            {
+                return this.x;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("value", "X coordinate must be 0 or more.");
+
+                this.x = value;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the position of the object on the Y axis.
+        /// </summary>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the value is less than zero.</exception>
+        public override int Y 
+        {
+            get
+            {
+                return this.y;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentOutOfRangeException("value", "Y coordinate must be 0 or more.");
+
+                this.y = value;
+            }
+        }
+        /// <summary>
+        /// Gets the the width of the object based on the position of its Characters objects.
+        /// </summary
+        public override int Width
+        {
+            get
+            {
+                int min = this.characters[0].X, max = this.characters[0].X;
+
+                for (int i = 1; i < this.characters.Length; i++)
+                {
+                    if (this.characters[i].X < min) min = this.characters[i].X;
+                    if (this.characters[i].X > max) min = this.characters[i].X;
+                }
+
+                return max - min;
+            }
+        }
+        /// <summary>
+        /// Gets the the height of the object based on the position of its Characters objects.
+        /// </summary>
+        public override int Height
+        {
+            get
+            {
+                int min = this.characters[0].Y, max = this.characters[0].Y;
+
+                for (int i = 1; i < this.characters.Length; i++)
+                {
+                    if (this.characters[i].X < min) min = this.characters[i].Y;
+                    if (this.characters[i].X > max) min = this.characters[i].Y;
+                }
+
+                return max - min;
+            }
+        }
+        /// <summary>
+        /// Gets or sets the function to invoke, when the dedicated attack button is pressed.
+        /// </summary>
+        public AttackEvent Attack
+        {
+            get
+            {
+                return this.attack;
+            }
+            set
+            {
+                this.attack = value;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        /// <summary>
+        /// Initializes a new Player object.
+        /// </summary>
+        /// <param name="x">The position of the object on the X axis.</param>
+        /// <param name="y">The position of the object on the Y axis.</param>
+        /// <param name="characters">The characters of</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when the either coordinate is less than zero.</exception>
+        public Player(int x, int y, AttackEvent attack, params Character[] characters)
+        {
+            this.X = x;
+            this.Y = y;
+            this.attack = attack;
+            this.characters = characters;
+        }
+        #endregion
+
+        #region Methods
+        /// <summary>
+        /// Adjusts the position of the object.
+        /// </summary>
+        /// <param name="x">The value by which to move the object on the X axis.</param>
+        /// <param name="y">The value by which to move the object on the X axis.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when either coordinate would be less than zero.</exception>
+        public override void Move(int x = 0, int y = 0) => Move(this, x, y);
+        /// <summary>
+        /// Sets the position of the object.
+        /// </summary>
+        /// <param name="x">The value to set the X coordinate to.</param>
+        /// <param name="y">The value to set the Y coordinate to.</param>
+        /// <exception cref="ArgumentOutOfRangeException">Thrown when either coordinate would is less than zero.</</exception>
+        public override void MoveTo(int x = 0, int y = 0) => MoveTo(this, x, y);
+        /// <summary>
+        /// Draw the character of the object.
+        /// </summary>
+        public override void Draw() => Draw(this);
+        /// <summary>
+        /// Draw the character of the object with a specified color.
+        /// </summary>
+        /// <param name="color">A ConsoleColor object to specifiy the color, the characters to draw with.</param>
+        public override void Draw(ConsoleColor color) => Draw(this, color);
+        /// <summary>
+        /// Clear the space occupied by the drawn character of the object.
+        /// </summary>
+        public override void CClear() => CClear(this);
+        /// <summary>
+        /// Process inputs for the keyboard.
+        /// </summary>
+        /// <param name="key">The key that was pressed.</param>
+        public void ProcessKey(ConsoleKey key)
+        {
+            switch (key)
+            {
+                case ConsoleKey.LeftArrow:
+                    this.Move(-1, 0);
+                    break;
+                case ConsoleKey.RightArrow:
+                    this.Move(1, 0);
+                    break;
+                case ConsoleKey.Spacebar:
+                    this.attack.Invoke(this);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Check whether the object and another occupy the same space.
+        /// </summary>
+        /// <param name="other">The other object to use for comparison</param>
+        /// <returns>True if the two object overlap, False otherwise.</returns>
+        public override bool Collides(object other) => Collides(this, other);
+        #endregion
+    }
+}
